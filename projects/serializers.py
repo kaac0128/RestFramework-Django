044c275158e92ef97c1b37bb643093e97484b0c8
kaac0128 <kaacLearningg@gmail.com>
2022-09-27 13:08:01 -0500

    RestFramework-Django Main

---
+++ projects/serializers.py
@@ -0,0 +1,10 @@
+from dataclasses import fields
+from pyexpat import model
+from rest_framework import serializers
+from .models import Project
+
+class ProjectSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Project
+		fields = ('id', 'title', 'description', 'technology', 'created_at')
+		read_only_fields = ('created_at', )
