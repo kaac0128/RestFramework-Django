532c1eacc4e50e50c40c2ca4304ba5bee67b4576
kaac0128 <kaacLearningg@gmail.com>
2022-09-29 13:37:08 -0500

    config render

--- projects/serializers.py
+++ projects/serializers.py
@@ -6,5 +6,5 @@ from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Project
-		fields = ('id', 'title', 'description', 'technology', 'created_at')
+		fields = ('id', 'username', 'description', 'email', 'created_at')
 		read_only_fields = ('created_at', )
